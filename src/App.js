243528fc3da8bcc32f6547ad205a23bb82cf705b
harissuresh03 <harissuresh03@gmail.com>
2026-07-10 23:25:00 +0800

    Removed minor error

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
     <>
       <ScrollToTop />
       <Navigation />
-      <ParticleBackground /> 
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
